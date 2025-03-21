fc730f4d60b1f08d95615f1e897ae086cb4573bd
kimia <doulabikimia@gmail.com>
2025-03-21 14:21:55 -0400

    I did until Q8

--- LogAnalyzer.java
+++ LogAnalyzer.java
@@ -5,15 +5,46 @@
  * @author David J. Barnes and Michael Kölling.
  * @version    2016.02.29
  */
+ /*
+  * Q1, 
+  * 10 14 18 
+  * 
+  * Q4,
+  * It is used often bcz it is used in fields,constructors 
+  * and also the first 2 methods
+  * 
+  * Q5 
+  * int[] counts; it has o be after the type and not before
+  * boolean[] occupied;
+  * occupied = new boolean[5000];
+  * 
+  * Q7 20 string objects are created by the following declaration
+  * 
+  * 
+  * 
+    */
+  
+
 public class LogAnalyzer
 {
     // Where to calculate the hourly access counts.
     private int[] hourCounts;
     // Use a LogfileReader to access the data.
     private LogfileReader reader;
-    //private Person[] people; 
+    //Q3 private Person[] people; 
+    //Q4
     private boolean[] vacant;
-
+    //Q5
+    private  boolean[] occupied;
+    int[] counts;
+    //Q6
+    double[] readings;
+    String[] urls;
+    //Q7 
+    String[] labels;
+    //Q8
+    double[] prices;
+    //TicketMachine[] machines;
     /**
      * Create an object to analyze hourly web accesses.
      */
@@ -24,6 +55,16 @@ public class LogAnalyzer
         hourCounts = new int[24];
         // Create the reader to obtain the data.
         reader = new LogfileReader();
+        //Q5
+        occupied = new boolean[5000];
+        //Q6
+        readings = new double[60];
+        urls = new String[90];
+        //machines = new TicketMachine[5];
+        //Q7 
+        labels = new String[20];
+        //Q8
+        prices = new double[50];
     }
 
     /**
